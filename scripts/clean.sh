0824370f90162f2c794f5c929a86f83975f0bc4b
kdotdot <carlos.gomeztapia@gmail.com>
2025-01-19 16:27:02 +0000

    Fwd ok?

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,3 +2,5 @@ rm -rf cerebra_atlas_python.egg-info/
 rm -rf .pytest_cache
 rm -rf dist build
 rm -rf ~/.cache/cerebra_atlas_python/*
+rm -rf ~/Library/Caches/cerebra_atlas_python/cerebra/*
+echo "OK"
