f4adcb1fb5ba1394d83f806b926c9db090d3fe74
kdotdot <carlos.gomeztapia@gmail.com>
2025-09-11 21:02:59 +0100

    BKP

--- examples/3d_plot.py
+++ examples/3d_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-from cerebra_atlas_python import CerebrA
+from cerebra_atlas_python import CerebrA, setup_logging
 
+setup_logging("DEBUG")
 cerebra = CerebrA()
-cerebra.plot_3d()
-plt.show()
+cerebra.plot_3d(colors="#ffffff")
+# plt.show()
